4a438f6b143d7aefd36653078bdabdeaa812a108
a0100019 <dldbqls0019@naver.com>
2024-09-26 10:07:40 +0900

    게시글 카드 구현, 더보기 기능

--- presentation/src/main/java/com/example/presentation/main/board/BoardScreen.kt
+++ presentation/src/main/java/com/example/presentation/main/board/BoardScreen.kt
@@ -1,9 +1,71 @@
 package com.example.presentation.main.board
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.presentation.model.main.board.BoardCardModel
+import com.example.presentation.theme.ConnectedTheme
 
 @Composable
-fun BoardScreen() {
-    Text(text = "BoardScreen")
+fun BoardScreen(
+    boardCardModels: List<BoardCardModel>,
+    onOptionClick: (BoardCardModel) -> Unit,
+    onReplyClick: (BoardCardModel) -> Unit
+) {
+    Surface {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            items(
+                count = boardCardModels.size,
+                key = { index -> boardCardModels[index].boardId },
+            ) {index ->
+                val boardCardModel = boardCardModels[index]
+                BoardCard(
+                    username = boardCardModel.username,
+                    images = boardCardModel.images,
+                    text = boardCardModel.text,
+                    onOptionClick = { onOptionClick(boardCardModel) },
+                    onReplyClick = { onReplyClick(boardCardModel) }
+                )
+            }
+
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BoardScreenPreview() {
+    ConnectedTheme {
+        BoardScreen(
+            boardCardModels = listOf(
+                BoardCardModel(
+                    boardId = 1,
+                    username = "aaaaa",
+                    images = listOf(),
+                    text = "aaaaa"
+                ),
+                BoardCardModel(
+                    boardId = 2,
+                    username = "aaaaa",
+                    images = listOf(),
+                    text = "aaaaa"
+                ),
+                BoardCardModel(
+                    boardId = 3,
+                    username = "aaaaa",
+                    images = listOf(),
+                    text = "aaaaa"
+                ),
+            ),
+            onOptionClick = {},
+            onReplyClick = {}
+        )
+    }
 }
